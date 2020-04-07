5c0ea2f4e2e71edfd8a0f1b2668a3a7b6f5e788d
zhf <haofei.zhong@pactera.com>
2020-04-07 15:17:16 +0800

    参数编号改为系统自动生成

--- pncs-indicator/pncs-indicator-manager/pncs-indicators-service/src/main/java/com/pactera/indicators/parameter/service/impl/IndParameterServiceImpl.java
+++ pncs-indicator/pncs-indicator-manager/pncs-indicators-service/src/main/java/com/pactera/indicators/parameter/service/impl/IndParameterServiceImpl.java
@@ -1,9 +1,11 @@
 package com.pactera.indicators.parameter.service.impl;
 
 import com.pactera.core.base.service.impl.BaseServiceImpl;
+import com.pactera.core.message.Message;
 import com.pactera.indicators.parameter.mapper.IndParameterMapper;
 import com.pactera.indicators.parameter.model.IndParameter;
 import com.pactera.indicators.parameter.service.IIndParameterService;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -21,4 +23,16 @@ public class IndParameterServiceImpl extends BaseServiceImpl<IndParameterMapper,
 
     @Resource
     private IndParameterMapper mapper;
+
+    @Override
+    public Message<IndParameter> save(IndParameter model) {
+        String parmCode = mapper.maxParmCode();
+        if (StringUtils.isBlank(parmCode)) {
+            model.setParmCode("M0000001");
+        } else {
+            model.setParmCode(parmCode.substring(0, 1) + StringUtils.leftPad((Integer.valueOf(parmCode.substring(1)) + 1) + "", 7, '0'));
+        }
+        logger.debug("生成参数编号：{}", model.getParmCode());
+        return super.save(model);
+    }
 }
