297050f96054648e6a1b845f4db88c02d12f5bf7
zhf <haofei.zhong@pactera.com>
2020-04-29 15:01:29 +0800

    维度编号改为系统自动生成

--- pncs-indicator/pncs-indicator-manager/pncs-indicators-service/src/main/java/com/pactera/indicators/dimension/service/impl/IndDimensionServiceImpl.java
+++ pncs-indicator/pncs-indicator-manager/pncs-indicators-service/src/main/java/com/pactera/indicators/dimension/service/impl/IndDimensionServiceImpl.java
@@ -1,10 +1,12 @@
 package com.pactera.indicators.dimension.service.impl;
 
 import com.pactera.core.base.service.impl.BaseServiceImpl;
+import com.pactera.core.message.Message;
 import com.pactera.indicators.dimension.mapper.IndDimensionMapper;
 import com.pactera.indicators.dimension.model.IndDimension;
 import com.pactera.indicators.dimension.service.IIndDimensionService;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -21,4 +23,16 @@ public class IndDimensionServiceImpl  extends BaseServiceImpl<IndDimensionMapper
 
     @Resource
     private IndDimensionMapper mapper;
+
+    @Override
+    public Message<IndDimension> save(IndDimension model) {
+        String dimCode = mapper.maxDimCode();
+        if (StringUtils.isBlank(dimCode)) {
+            model.setDimCode("00000001");
+        } else {
+            model.setDimCode(dimCode.substring(0, 1) + StringUtils.leftPad((Integer.valueOf(dimCode.substring(1)) + 1) + "", 7, '0'));
+        }
+        logger.debug("生成维度编号：{}", model.getDimCode());
+        return super.save(model);
+    }
 }
